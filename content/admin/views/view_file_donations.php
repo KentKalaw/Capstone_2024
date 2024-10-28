836d75129f2fadde19af5d8d075b914bcc8e3b84
KentKalaw <kentfranciskalaw@gmail.com>
2024-10-29 01:19:33 +0800

    fixed: id

--- content/admin/views/view_file_donations.php
+++ content/admin/views/view_file_donations.php
@@ -1,17 +1,17 @@
 <?php
 include('../../connect.php');
-$id =$_GET['id'];
-$sql = "SELECT * FROM donations";
-                $result = $conn->query($sql);
-                    while($row = $result->fetch_assoc()) {
-                    $donation_id = $row['id'];
-                    $fullname = $row['fullname'];
-                    $email = $row['email'];
-                    $amount = $row['amount'];
-                    $number = $row['contact'];
-                    $image = $row['image'];
-                    $date = $row['date'];
-                }
+$donation_id = $_GET['id'];
+$sql = "SELECT * FROM donations WHERE id = '$donation_id'";
+    $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+        $donation_id = $row['id'];
+        $fullname = $row['fullname'];
+        $email = $row['email'];
+        $amount = $row['amount'];
+        $number = $row['contact'];
+        $image = $row['image'];
+        $date = $row['date'];
+    }
 ?>
 <h2>Attachment</h2>
 <hr style="width:500px">
